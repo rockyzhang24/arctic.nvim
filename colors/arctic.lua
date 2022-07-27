5b4d6f9b3d922798984860210936875a724cb420
Tyler Fox <tyler@blur.com>
2022-07-27 16:56:41 -0700

    Move files around to make requiring easier

--- colors/arctic.lua
+++ colors/arctic.lua
@@ -14,7 +14,7 @@ vim.g.colors_name = 'arctic'
 --
 -- The performance impact of this call can be measured in the hundreds of
 -- *nanoseconds* and such could be considered "production safe".
-package.loaded['lush_theme.arctic'] = nil
+package.loaded['arctic.lush_theme'] = nil
 
 -- include our theme file and pass it to lush to apply
-require('lush')(require('lush_theme.arctic'))
+require('lush')(require('arctic.lush_theme'))
